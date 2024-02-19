ecacefaa594ae022a87a79d35f200eeaeda2025c
Mounir el fahsi <elfahssimounir7@gmail.com>
2024-02-19 13:21:39 +0100

    ff

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,45 +1,17 @@
 #!/usr/bin/python3
-""" Script uses JSONPlaceholder API to get information about an employee's tasks """
-
+""" Script that uses JSONPlaceholder API to get information about employee """
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
+if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
 
-    # Fetch user details
-    user_response = requests.get(f'{url}users/{employee_id}')
-    if user_response.status_code != 200:
-        print("Error: Unable to fetch employee details.")
-        return
+    user = requests.get(f'{url}users/{sys.argv[1]}').json()
+    print(f"Employee {user.get('name')} is done with tasks", end="")
 
-    user = user_response.json()
-
-    # Fetch tasks for the user
-    tasks_response = requests.get(f'{url}todos?userId={employee_id}')
-    if tasks_response.status_code != 200:
-        print("Error: Unable to fetch employee tasks.")
-        return
-
-    tasks = tasks_response.json()
+    tasks = requests.get(f'{url}todos?userId={sys.argv[1]}').json()
     completed_tasks = [task for task in tasks if task.get('completed')]
 
-    # Display employee's progress
-    print(f"Employee {user.get('name')} is done with tasks ({len(completed_tasks)}/{len(tasks)}):")
+    print(f"({len(completed_tasks)}/{len(tasks)}):")
     for task in completed_tasks:
-        print(f"\t{task.get('title')}")
-
-if __name__ == "__main__":
-    # Check if an argument is provided
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-
-    # Check if the provided argument is a valid integer
-    if not employee_id.isdigit():
-        print("Error: Employee ID must be an integer.")
-        sys.exit(1)
-
-    get_employee_todo_progress(employee_id)
+        print(f"\t {task.get('title')}")
