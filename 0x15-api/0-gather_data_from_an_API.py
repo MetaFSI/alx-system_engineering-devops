e352e6f3df30fc662843c88edb59d53802604261
Mounir el fahsi <elfahssimounir7@gmail.com>
2024-02-19 13:29:05 +0100

    new change

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,24 +2,30 @@
 """ Script that uses JSONPlaceholder API get informations about employee """
 import requests
 import sys
-
+import csv
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
 
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
-    json_o = res.json()
-    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
-
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    res = requests.get(todos)
+    user_id = sys.argv[1]
+    user_url = '{}users/{}'.format(url, user_id)
+    res = requests.get(user_url)
+    user_info = res.json()
+    
+    todos_url = '{}todos?userId={}'.format(url, user_id)
+    res = requests.get(todos_url)
     tasks = res.json()
-    l_task = []
-    for task in tasks:
-        if task.get('completed') is True:
-            l_task.append(task)
 
-    print("({}/{}):".format(len(l_task), len(tasks)))
-    for task in l_task:
-        print("\t {}".format(task.get("title")))
+    user_name = user_info.get('username')
+
+    csv_filename = '{}.csv'.format(user_id)
+
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in tasks:
+            task_completed = "True" if task.get('completed') else "False"
+            task_title = task.get('title')
+            csv_writer.writerow([user_id, user_name, task_completed, task_title])
+
+    print("CSV file '{}' has been created successfully.".format(csv_filename))
